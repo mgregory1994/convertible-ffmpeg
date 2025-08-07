18cec9049c1ceb69dd8f11e245c9f0b5b43d746f
mgregory1994 <77855698+mgregory1994@users.noreply.github.com>
2025-08-06 18:52:21 -0700

    Update package imports
    Adds tasks and encoder queue imports.

--- ffmpeg/__init__.py
+++ ffmpeg/__init__.py
@@ -1,5 +1,24 @@
-from .errors import FFmpegAlreadyExecuted, FFmpegError, FFmpegFileNotFound, FFmpegInvalidCommand, FFmpegUnsupportedCodec
+from .errors import (
+    FFmpegAlreadyExecuted,
+    FFmpegError,
+    FFmpegFileNotFound,
+    FFmpegInvalidCommand,
+    FFmpegUnsupportedCodec,
+)
 from .ffmpeg import FFmpeg
 from .progress import Progress
+from .tasks import (
+    MediaTask,
+    FolderTask,
+    VideoStream,
+    AudioStream,
+    VideoSettings,
+    VideoFilters,
+    AudioSettings,
+    AudioFilters,
+    SubtitleSettings,
+    FFmpegHelper
+)
+from .encoder_queue import TaskQueue, HwaQueue, WatchFolderQueue
 
 __version__ = "2.0.12"
